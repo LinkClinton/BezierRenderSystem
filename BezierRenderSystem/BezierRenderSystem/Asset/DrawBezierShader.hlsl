63cc33cd2b7000fd7d3aa34a988f4a833040fe44
Link Clinton <LinkClinton@outlook.com>
2019-05-22 20:10:01 +0800

    support msaa

--- BezierRenderSystem/BezierRenderSystem/Asset/DrawBezierShader.hlsl
+++ BezierRenderSystem/BezierRenderSystem/Asset/DrawBezierShader.hlsl
@@ -2,6 +2,7 @@
 
 #define eps 1e-8
 #define pi 3.1415926535898
+#define no_root -1
 
 cbuffer Transform : register(b0)
 {
@@ -34,9 +35,9 @@ void solve_equation(
     float a, float b, float c, float d, 
     out float root0, out float root1, out float root2)
 {
-    root0 = 0;
-    root1 = 0;
-    root2 = 0;
+	root0 = no_root;
+	root1 = no_root;
+	root2 = no_root;
 
     if (abs(a) < eps) { //Quadratic case, bx^2 + cx + d = 0
         if (abs(b) < eps) { //Linear case, cx + d = 0
@@ -113,6 +114,46 @@ float2 bezier_curve_point(float2 A, float2 B, float2 C, float t)
     return A * t * t + B * t + C;
 }
 
+float msaa_sample(float2 position, float alpha) 
+{
+	//solve equation (-2A^2)t^3 + (-3AB)t^2 + (2AP - 2AC - B^2)t + B(P - C) = 0
+    //A = (-2A^2) = coefficient0.x
+    //B = (-3AB) = coefficient0.y
+    //C = (2AP - 2AC - B^2) = 2 * dot(coefficient1.xy, project.xy) + coefficient0.z
+    //D =  B(P - C) = dot(coefficient1.zw, project.xy) + coefficient0.w
+
+	float A = coefficient0.x;
+	float B = coefficient0.y;
+	float C = dot(coefficient1.xy, position.xy) * 2 + coefficient0.z;
+	float D = dot(coefficient1.zw, position.xy) + coefficient0.w;
+
+	float root0 = no_root;
+	float root1 = no_root;
+	float root2 = no_root;
+
+	solve_equation(A, B, C, D, root0, root1, root2);
+
+	if (root0 >= 0 && root0 <= 1.0f) {
+		float2 Q = bezier_curve_point(coefficient1.xy, coefficient1.zw, coefficient2.xy, root0);
+
+		if (distance(position.xy, Q) <= coefficient2.z) return alpha;
+	}
+
+	if (root1 >= 0 && root1 <= 1.0f) {
+		float2 Q = bezier_curve_point(coefficient1.xy, coefficient1.zw, coefficient2.xy, root1);
+
+		if (distance(position.xy, Q) <= coefficient2.z) return alpha;
+	}
+
+	if (root2 >= 0 && root2 <= 1.0f) {
+		float2 Q = bezier_curve_point(coefficient1.xy, coefficient1.zw, coefficient2.xy, root2);
+
+		if (distance(position.xy, Q) <= coefficient2.z) return alpha;
+	}
+
+	return 0;
+}
+
 Output vs_main(
 	float3 position : POSITION,
 	float2 texcoord : TEXCOORD,
@@ -134,43 +175,14 @@ float4 ps_main(
 	float2 texcoord : TEXCOORD,
 	float4 color : COLOR) : SV_TARGET
 {
-    //return float4(1, 0, 0, 1);
-    //solve equation (-2A^2)t^3 + (-3AB)t^2 + (2AP - 2AC - B^2)t + B(P - C) = 0
-    //A = (-2A^2) = coefficient0.x
-    //B = (-3AB) = coefficient0.y
-    //C = (2AP - 2AC - B^2) = 2 * dot(coefficient1.xy, project.xy) + coefficient0.z
-    //D =  B(P - C) = dot(coefficient1.zw, project.xy) + coefficient0.w
+	float2 center = project.xy;
 
-    float A = coefficient0.x;
-    float B = coefficient0.y;
-    float C = dot(coefficient1.xy, project.xy) * 2 + coefficient0.z;
-    float D = dot(coefficient1.zw, project.xy) + coefficient0.w;
+	if (msaa_sample(center, color.a) == 0) discard;
 
-    float root0 = 0;
-    float root1 = 0;
-    float root2 = 0;
+	float sample0 = msaa_sample(center + float2(0.25f, 0.25f), color.a);
+	float sample1 = msaa_sample(center + float2(0.25f, -0.25f), color.a);
+	float sample2 = msaa_sample(center + float2(-0.25f, 0.25f), color.a);
+	float sample3 = msaa_sample(center + float2(-0.25f, -0.25f), color.a);
 
-    solve_equation(A, B, C, D, root0, root1, root2);
-
-    if (root0 >= 0 && root0 <= 1.0f){
-        float2 Q = bezier_curve_point(coefficient1.xy, coefficient1.zw, coefficient2.xy, root0);
-        
-        if (distance(project.xy, Q) <= coefficient2.z) return color;
-    }
-
-    if (root1 >= 0 && root1 <= 1.0f){
-        float2 Q = bezier_curve_point(coefficient1.xy, coefficient1.zw, coefficient2.xy, root1);
-        
-        if (distance(project.xy, Q) <= coefficient2.z) return color;
-    }
-
-    if (root2 >= 0 && root2 <= 1.0f){
-        float2 Q = bezier_curve_point(coefficient1.xy, coefficient1.zw, coefficient2.xy, root2);
-        
-        if (distance(project.xy, Q) <= coefficient2.z) return color;
-    }
-
-    discard;
-
-    return color;
+	return float4(color.xyz, (sample0 + sample1 + sample2 + sample3) * 0.25f);
 }
